05c40c8d1edc0caa0b902da5ee03c671e2960cc3
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-11-24 20:21:42 -0500

    Created test book and label

---
+++ Widget.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtCore, QtWidgets, QtGui
+import Book
+import random
+
+class Main_Widget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        book1 = Book.Book("Foundation", "Isaac Asimov", "1951", "Science Fiction", "320")
+
+        label = QtWidgets.QLabel(self)
+        label.setText(book1.print_title())
+
